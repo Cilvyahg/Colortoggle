81e1c3a5461c9f05d82481d0e88a6ca13c2cbc08
Cilvya <cilvya.hg@gmail.com>
2022-07-22 15:44:48 +0200

    radiobutton query

--- ColorToggleAssign/main.js
+++ ColorToggleAssign/main.js
@@ -1,14 +1,32 @@
-console.log('hello');
+
 
 const enterHamburger = document.querySelector('.main-nav__hamburger');
 const sideNavToggle = document.querySelector('.side-nav__list');
+
 const homeToggle = document.querySelector('.home');
-const pinkToggle = document.querySelector('#pink');
-const yellowToggle = document.querySelector('#yellow');
-const greenToggle = document.querySelector('#green');
-const blueToggle = document.querySelector('#blue');
-const orangeToggle = document.querySelector('#orange');
-const redToggle = document.querySelector('#red');
+
+const pinkToggle = document.querySelector('.pink');
+const pinkRadio = document.querySelector('#pink');
+
+
+const yellowToggle = document.querySelector('.yellow');
+const yellowRadio = document.querySelector('#yellow');
+
+
+const greenToggle = document.querySelector('.green');
+const greenRadio = document.querySelector('#green');
+
+
+const blueToggle = document.querySelector('.blue');
+const blueRadio = document.querySelector('#blue');
+
+const orangeToggle = document.querySelector('.orange');
+const orangeRadio = document.querySelector('#orange');
+
+const redToggle = document.querySelector('.red');
+const redRadio = document.querySelector('#red');
+
+
 const colorPicker = document.querySelector('.colordisplay__text');
 
 
@@ -17,19 +35,19 @@ enterHamburger.addEventListener('mouseenter', () => {
 });
 
 
-const colorToggler = function (colorItem, bgc, innertext) {
+const colorToggler = function (colorItem, bgc, innertext, radioBtn) {
   colorItem.addEventListener('click', () => {
     document.body.style.backgroundColor = bgc;
     colorPicker.innerText = innertext;
-    colorItem.checked = true;
+    radioBtn.checked = true;
     sideNavToggle.style.visibility = 'hidden';
   });
 };
 
-colorToggler(redToggle, '#ff0000', 'red');
-colorToggler(orangeToggle, '#f0a442', 'orange');
-colorToggler(blueToggle, '#90b1f3', 'blue');
-colorToggler(greenToggle, '#6efd6d', 'green');
-colorToggler(yellowToggle, '#ecef60', 'yellow');
-colorToggler(pinkToggle, '#e389d3', 'pink');
 colorToggler(homeToggle, '#cdcdcd', 'grey');
+colorToggler(pinkToggle, '#e389d3', 'pink', pinkRadio);
+colorToggler(yellowToggle, '#ecef60', 'yellow', yellowRadio);
+colorToggler(greenToggle, '#6efd6d', 'green', greenRadio);
+colorToggler(blueToggle, '#90b1f3', 'blue', blueRadio);
+colorToggler(orangeToggle, '#f0a442', 'orange', orangeRadio);
+colorToggler(redToggle, '#ff0000', 'red', redRadio);
